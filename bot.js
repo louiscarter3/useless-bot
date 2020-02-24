5122d4a854ee98cc6f6631ad17d8a1b7f5e5b2a3
Louis C <lc621@kent.ac.uk>
2020-02-24 14:52:30 +0000

    the bot now loves the beautiful game!

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
-  return "Sorry Dave, I cannot do that."
+  else if  (query.indexOf("football") != -1)
+    return "I love the footy!";
+  return "Sorry Dave, I cannot do that.";
 }
 
 
@@ -11,7 +13,7 @@ function handleSayClick() {
   // which we are using for showing all the conversation history
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  
+
   // Get answer for the given query and show the query & answer
   var answer = answerQuery(message)
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
